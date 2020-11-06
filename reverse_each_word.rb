4924cf0ab61d8aa2cc7cd6a269a98f0c2864715a
Jacob Tobias <jtobias811@gmail.com>
2020-11-06 16:49:35 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+
+def reverse_each_word(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  new_array = array.collect do |words|
+    words.reverse
+  end
+  new_array.join(" ")
+end
